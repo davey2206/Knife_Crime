5ffc3ade2ec002efb8ffd3797f1296a335e38c58
Tmoemes <meesvanleusden@gmail.com>
2023-05-10 16:15:59 +0200

    added simple model and animations

--- Assets/Group_1/Assets/Character Model/KnifeAttacks.cs
+++ Assets/Group_1/Assets/Character Model/KnifeAttacks.cs
@@ -15,9 +15,20 @@ public class KnifeAttacks : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButtonDown(0) && !animator.IsInTransition(0))
         {
             animator.Play("Stab");
+
+        }
+
+        if(Input.GetKeyDown(KeyCode.E) && !animator.IsInTransition(0))
+        {
+            animator.CrossFade("Dodge_Right",0.2f,0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q) && !animator.IsInTransition(0))
+        {
+            animator.CrossFade("Dodge_Left", 0.2f,0);
         }
     }
 }
