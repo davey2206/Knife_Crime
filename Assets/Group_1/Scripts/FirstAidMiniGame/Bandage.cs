a74e0cfc8c5d06b1fff347bec1c0f64eb53001ae
Ben <bendevriesxd@gmail.com>
2023-05-17 11:54:24 +0200

    Applying 2 bandages with swipe

--- Assets/Group_1/Scripts/FirstAidMiniGame/Bandage.cs
+++ Assets/Group_1/Scripts/FirstAidMiniGame/Bandage.cs
@@ -6,15 +6,18 @@ public class Bandage : MonoBehaviour
 {
     private Vector2 swipeStartPos;
     private Vector2 swipeEndPos;
+    private int bandageIndex = 0;
+    private int amountBandagesApplied = 0;
     public float minSwipeDistance = 50f;
     public GameObject[] bandage;
-    public int bandageIndex = 0;
+    public GameObject appliedBandage;
 
     void Start()
     {
         bandage[0].SetActive(false);
         bandage[1].SetActive(false);
         bandage[2].SetActive(false);
+        appliedBandage.SetActive(false);
     }
 
     // Update is called once per frame
@@ -29,6 +32,10 @@ public class Bandage : MonoBehaviour
         {
             swipeEndPos = Input.mousePosition;
             CheckSwipeGesture();
+            if (bandageIndex == 3)
+            {
+                StartCoroutine(CheckBandage());
+            }
         }
     }
 
@@ -54,11 +61,11 @@ public class Bandage : MonoBehaviour
             else if (swipeDirection.y < -0.5f)
             {
                 Debug.Log("Swipe Down");
-                if (bandageIndex > 0)
-                {
-                    bandageIndex--;
-                    bandage[bandageIndex].SetActive(false);
-                }
+                //if (bandageIndex > 0)
+                //{
+                //    bandageIndex--;
+                //    bandage[bandageIndex].SetActive(false);
+                //}
                 // Implement your desired action for swipe down
             }
             else if (swipeDirection.x > 0.5f)
@@ -72,5 +79,30 @@ public class Bandage : MonoBehaviour
                 // Implement your desired action for swipe left
             }
         }
+    }
+
+    public void ResetBandage()
+    {
+        if (amountBandagesApplied == 1)
+        {
+            bandageIndex = 0;
+            bandage[0].SetActive(false);
+            bandage[1].SetActive(false);
+            bandage[2].SetActive(false);
+            appliedBandage.SetActive(true);
+        }
+        else
+        {
+            bandage[0].gameObject.GetComponent<Renderer>().material.color = Color.red;
+            bandage[1].gameObject.GetComponent<Renderer>().material.color = Color.red;
+            bandage[2].gameObject.GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
+
+    IEnumerator CheckBandage()
+    {
+        yield return new WaitForSeconds(2);
+        amountBandagesApplied++;
+        ResetBandage();
     }
 }
