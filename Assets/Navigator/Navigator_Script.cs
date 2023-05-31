fb91b4f594f96033c6d64b10ce280724d999f6c7
davey2206 <davey2206@live.nl>
2023-05-31 12:51:11 +0200

    added navigator trail

---
+++ Assets/Navigator/Navigator_Script.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Navigator_Script : MonoBehaviour
+{
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Transform moveTo;
+    [SerializeField] Transform player;
+
+    void OnEnable()
+    {
+        agent.Warp(new Vector3(player.localPosition.x, 0.8f, player.localPosition.z));
+        agent.SetDestination(moveTo.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(moveTo.position);
+
+        float dist = agent.remainingDistance;
+        if (dist != Mathf.Infinity && agent.pathStatus == NavMeshPathStatus.PathComplete && agent.remainingDistance == 0)
+        {
+            agent.Warp(new Vector3(player.localPosition.x, 0.8f, player.localPosition.z));
+            agent.SetDestination(moveTo.position);
+        }
+    }
+}
