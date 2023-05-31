409ca84a127fc4c32050a7a2a46b7080f0422eb6
GabiHr <gabrielkahr@gmail.com>
2023-05-31 15:00:44 +0200

    automatic dialog system
    Added a timer to the dialogue system so the player doesn't need to click in order to continue. This way the player can just follow the npc.

--- Assets/Group_2/Scripts/Gabi/Dialog Manager.cs
+++ Assets/Group_2/Scripts/Gabi/Dialog Manager.cs
@@ -17,28 +17,49 @@ public class DialogManager : MonoBehaviour
     private int activeMessage = 0;
     private static bool isActive = false;
 
+    // timer 
+    [SerializeField] private float afterDisplayTimer;
+    [HideInInspector] private bool startTimer;
+    private float timer;
+ 
     public void OpenDialogue(Message[] messages, Actor[] actors)
     {
+        Debug.Log("Open Dialog");
+
         currentMessages = messages;
         currentActors = actors;
         activeMessage = 0;
         isActive = true;
+
         animator.SetBool("IsOpen", true);
         DisplayMessage();
+
     }
 
     public void DisplayMessage()
     {
+        Debug.Log("Start");
+
+        //display messages
         Message messageToDisplay = currentMessages[activeMessage];
         messageText.SetText(messageToDisplay.message);
 
         Actor actorToDisplay = currentActors[messageToDisplay.actorId];
         actorName.SetText(actorToDisplay.name);
 
+        //timer
+        
+        timer = afterDisplayTimer;
+        startTimer = true;
+
+        Debug.Log("End");
+
     }
 
     public void NextMessage()
     {
+        startTimer = false;
+        Debug.Log("next" + startTimer);
         activeMessage++;
         if (activeMessage < currentMessages.Length)
         {
@@ -55,10 +76,22 @@ public class DialogManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && isActive) 
+        if(isActive) 
         {
+            if (startTimer)
+            {
+                timer -= Time.deltaTime;
+                if (timer <= 0)
+                {
+                    timer = 0;
+                    Debug.Log("timer" + startTimer);
+                    NextMessage();  // show message after displaying the message + timer
+                    
+                }
 
-            NextMessage();
+            }
+            else { Debug.Log("nahhhh"); }
+
         }
         
     }
