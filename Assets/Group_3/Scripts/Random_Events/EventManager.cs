f734843a29172591eaf168d2a49547ede08692c0
BenDBrown <benjamin.d.brown.2002@gmail.com>
2023-06-07 08:43:45 +0200

    cursor fix for menu + event manager fix

--- Assets/Group_3/Scripts/Random_Events/EventManager.cs
+++ Assets/Group_3/Scripts/Random_Events/EventManager.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using static UnityEditor.Searcher.Searcher.AnalyticsEvent;
 
 public class EventManager : MonoBehaviour
 {
